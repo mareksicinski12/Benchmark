b69df447658880c243dd49e5c56a1001a59b0bfb
Marek Sicinski <119501979+mareksicinski12@users.noreply.github.com>
2024-12-03 17:44:16 +0100

    Add files via upload

---
+++ query12.sql
@@ -0,0 +1,21 @@
+WITH AvgPostScore AS (
+    SELECT 
+        AVG(p.Score) AS AverageScore
+    FROM 
+        posts p
+)
+SELECT 
+    u.Id AS UserId,
+    u.DisplayName AS UserDisplayName,
+    p.Id AS PostId,
+    p.Title AS PostTitle,
+    p.Score AS PostScore
+FROM 
+    users u
+	JOIN posts p ON u.Id = p.OwnerUserId
+	JOIN AvgPostScore aps ON p.Score > aps.AverageScore
+WHERE 
+    u.Reputation > 10000  
+	AND p.Title IS NOT NULL
+ORDER BY 
+    u.Reputation DESC, p.Score DESC;
