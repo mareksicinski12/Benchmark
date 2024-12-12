3246f54983a923efd2abc01745a0f6a6704043e7
Marek Sicinski <119501979+mareksicinski12@users.noreply.github.com>
2024-12-12 11:59:49 +0100

    Update query10_base_subquery.sql

--- Basecases/query10_basecases/query10_base_subquery.sql
+++ Basecases/query10_basecases/query10_base_subquery.sql
@@ -1,1 +1,43 @@
-
+--EXPLAIN ANALYZE
+SELECT
+	u.Id,
+	u.DisplayName,
+	bc.NumberOfBadges,
+	p.Title,
+	p.CreationDate
+FROM 
+	users u	
+	JOIN (
+		SELECT
+			b.UserId,
+			COUNT(b.Id) AS NumberOfBadges
+		FROM
+			badges b 
+		GROUP BY 
+			b.UserId
+	) bc ON u.Id = bc.UserId
+	JOIN (
+		SELECT 
+        p.OwnerUserId,
+        COUNT(p.Id) AS AcceptedAnswerCount
+    	FROM 
+        posts p
+    	WHERE 
+        	p.PostTypeId = 2 
+        	AND p.Id IN (
+            	SELECT AcceptedAnswerId 
+            	FROM posts
+            	WHERE AcceptedAnswerId IS NOT NULL
+        	) 
+    	GROUP BY 
+        	p.OwnerUserId
+	) ua ON u.Id = ua.OwnerUserId
+	JOIN 
+		posts p ON u.Id = p.OwnerUserId
+WHERE 
+	bc.NumberOfBadges > 10 
+	AND ua.AcceptedAnswerCount > 5
+	AND p.Title IS NOT NULL
+ORDER BY 
+    u.Reputation DESC, 
+	  p.CreationDate DESC
