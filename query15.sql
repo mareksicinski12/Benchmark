68be95dcb0d1eb1c587da4adf926b72eb43099e7
Marek Sicinski <119501979+mareksicinski12@users.noreply.github.com>
2025-01-09 12:21:25 +0100

    Add files via upload

---
+++ query15.sql
@@ -0,0 +1,25 @@
+--query15
+WITH RECURSIVE UserInteractions AS (
+    SELECT 
+		c.UserId AS User1, p.OwnerUserId AS User2
+    FROM 
+		comments c
+    JOIN 
+		posts p ON c.PostId = p.Id
+    WHERE 
+		c.UserId IS NOT NULL 
+		AND p.OwnerUserId IS NOT NULL
+	
+    UNION 
+	
+    SELECT 
+		ui.User2 AS User1, p.OwnerUserId AS User2
+    FROM 
+		UserInteractions ui
+    JOIN 
+		posts p ON ui.User2 = p.OwnerUserId
+)
+SELECT  User1, User2
+FROM UserInteractions
+WHERE User1 = 13
+GROUP BY User1, User2
