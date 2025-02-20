a1ed8dcd6b7c46cb3ef13a853f7ecf722eae9bbb
Marek Sicinski <119501979+mareksicinski12@users.noreply.github.com>
2025-02-20 21:25:12 +0100

    Update query10_base_having.sql

--- Basecases/query10_basecases/query10_base_having.sql
+++ Basecases/query10_basecases/query10_base_having.sql
@@ -3,17 +3,12 @@ SELECT
     u.DisplayName,
     COUNT(b.Id) AS NumberOfBadges,
     u.Reputation,
-    COUNT(DISTINCT p2.Id) AS AcceptedAnswerCount,
     p.Title AS PostTitle,
-    p.ViewCount,
-    p.CreationDate
+    p.ViewCount
 FROM users u
 JOIN badges b ON u.Id = b.UserId
 JOIN posts p ON u.Id = p.OwnerUserId
-LEFT JOIN posts p2 ON p2.OwnerUserId = u.Id 
-    AND p2.PostTypeId = 2 
-    AND p2.Id IN (SELECT AcceptedAnswerId FROM posts WHERE AcceptedAnswerId IS NOT NULL)
 WHERE p.Title IS NOT NULL
-GROUP BY u.Id, u.DisplayName, u.Reputation, p.Title, p.ViewCount, p.CreationDate
-HAVING COUNT(b.Id) > 50 AND COUNT(DISTINCT p2.Id) > 5
-ORDER BY u.Reputation DESC, p.CreationDate DESC;
+GROUP BY u.Id, u.DisplayName, u.Reputation, p.Title, p.ViewCount
+HAVING COUNT(b.Id) > 50
+ORDER BY u.Reputation DESC;
